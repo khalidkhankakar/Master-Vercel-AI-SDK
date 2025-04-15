2f7a540401f5359a698182a901301a6805c3be10
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-15 20:39:35 +0500

    Adding different models

--- src/examples/01_generate_text.ts
+++ src/examples/01_generate_text.ts
@@ -1,13 +1,18 @@
-import { anthropic } from "@ai-sdk/anthropic";
+import { createDeepSeek } from '@ai-sdk/deepseek';
 import { generateText } from "ai";
 
-const model = anthropic("claude-3-5-haiku-latest");
-
+// const anthropic = createAnthropic({
+//   apiKey: process.env.ANTHROPIC_API_KEY,
+// });
+const deepseek = createDeepSeek({
+  apiKey: process.env.DEEPSEEK_API_KEY ?? '',
+});
 export const answerMyQuestion = async (
   prompt: string,
 ) => {
+  console.log(process.env.DEEPSEEK_API_KEY)
   const { text } = await generateText({
-    model,
+    model: deepseek('deepseek-chat'),
     prompt,
   });
 
