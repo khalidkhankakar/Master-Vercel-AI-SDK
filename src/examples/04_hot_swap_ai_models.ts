40810b62a278b91caaa15b6e2d99127ae878aa81
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-04-19 13:56:30 +0500

    🎉 Add example for hot swapping between Mistral and DeepSeek models

---
+++ src/examples/04_hot_swap_ai_models.ts
@@ -0,0 +1,30 @@
+import { generateText, LanguageModel } from "ai";
+import { deepseek, mistral } from "../model";
+
+const ask = async (
+    prompt: string,
+    model: LanguageModel
+) => {
+    const { text } = await generateText({
+        prompt,
+        model,
+    })
+
+    return text
+}
+
+
+const mistralRes = await ask(
+    "How many planets are in our solar system?",
+    mistral('Mistral-large-2411')
+)
+console.log("Mistral response: ", mistralRes)
+
+
+const DeepSeekRes = await ask(
+    "How many planets are in our solar system?",
+    deepseek('deepseek/DeepSeek-V3-0324')
+)
+console.log("DeepSeek response: ", DeepSeekRes)
+
+
