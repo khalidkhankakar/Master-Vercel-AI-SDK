fce7a5d4564887ed420bf42e1ceccea6bbf03132
khalidkhankakar <khalidkhankakar2468@gmail.com>
2025-11-28 21:40:05 +0500

    using own cloud ollama models

--- chat-agent/src/app/api/chat/route.ts
+++ chat-agent/src/app/api/chat/route.ts
@@ -1,19 +1,29 @@
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { th } from 'date-fns/locale';
 // import { groq } from '@ai-sdk/groq';
 import { createOllama } from 'ollama-ai-provider-v2';
 
 
+// https://ollama-56562450710.europe-west1.run.app/api
+// qwen3-vl:4b
+
+
+// https://ollama-new-56562450710.me-central1.run.app/
+// deepseek-r1:1.5b
+
 const ollama = createOllama({
-  baseURL:"http://127.0.0.1:11434/api",
+  baseURL:"https://ollama-new-56562450710.me-central1.run.app/api",
 })
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
-  const model = ollama('qwen3:1.7b')
+  const model = ollama('deepseek-r1:1.5b')
   const result = streamText({
+
     model,
     messages: convertToModelMessages(messages),
+    // providerOptions: { ollama:{ thi }
   });
 
   return result.toUIMessageStreamResponse();
